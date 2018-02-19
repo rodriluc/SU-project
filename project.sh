8babae01f46b453d45e2cdf9d8010de46847bb0c
rodriluc <luciest.rodriguez@gmail.com>
2018-02-19 09:36:58 +0100

    Add bash script

---
+++ project.sh
@@ -0,0 +1,20 @@
+cd
+mkdir molecularproject
+cd molecularproject
+
+echo -e "This is a template, follow the steps: " > readme.txt
+
+mkdir paper
+cd paper
+echo "Notes for individual project" > notes.txt
+cd ..
+mkdir general_scripts
+mkdir datasets
+mkdir codes
+cd codes
+echo "#This is a sample code" > samplecode.py
+cd ..
+mkdir bin
+mkdir output
+cd
+
