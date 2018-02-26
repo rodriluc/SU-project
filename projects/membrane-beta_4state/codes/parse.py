d46e5859538593dd3b851775f8d51f5b5ff3f742
rodriluc <luciest.rodriguez@gmail.com>
2018-02-26 12:19:10 +0100

    finished binary and numerical code conversion

--- projects/membrane-beta_4state/codes/parse.py
+++ projects/membrane-beta_4state/codes/parse.py
@@ -12,6 +12,8 @@ from sklearn.feature_extraction import DictVectorizer
 # Opens the file and appends each feature of the file into 3 lists then dictionary
 ###################################################################################
 
+testfile = "D:/SU-project/projects/membrane-beta_4state/datasets/parsetest"
+
 def parseBeta(infile):
 
     dictAll = {}
@@ -47,42 +49,48 @@ def inputSVM(infile):
 
 #AA letters: A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y
 
-    AA_seq={'A':'1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'C':'0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'D':'0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'E':'0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'F':'0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'G':'0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'H':'0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0',
-            'I':'0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0',
-            'K':'0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0',
-            'L':'0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0',
-            'M':'0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0',
-            'N':'0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0',
-            'P':'0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0',
-            'Q':'0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0',
-            'R':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0',
-            'S':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0',
-            'T':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0',
-            'V':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0',
-            'W':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0',
-            'Y':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1',
-            'X':'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'} #extra residue for window
- 
-    structure_ = {  'i':'0', #inner
-                    'P':'1',
-                    'L':'2',
-                    'o':'3'}#outer
+    AA_seq_dict={'A':[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #'' to []
+            'C':[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'D':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'E':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'F':[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'G':[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'H':[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            'I':[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+            'K':[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+            'L':[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+            'M':[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+            'N':[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            'P':[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+            'Q':[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+            'R':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+            'S':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+            'T':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+            'V':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+            'W':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            'Y':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+            'X':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]} #extra residue for window/ not being used for my new verison of code
+    #print (AA_seq_dict)
+    structure_dict = {'i':0, #inner
+                      'P':1,
+                      'L':2,
+                      'o':3}#outer
                     
 ###################################################################################
 # Convert into SVM input vector: Sliding Window Input
 ###################################################################################
-    
+    #dictAll = {}
     listID = []
     listaa = []
     listTop = []
-    AAlist = []
-    Toplist = []
+    AAlist = [] 
+    #Toplist = [] 
+    final_AAlist = [] #final
+    final_Toplist = [] #final
+    #listAA = list(AA_seq)
+    listaa_window = []
+    listTop_window = []
+    encwindow_list = []
   
     with open(infile) as pf:
         #lines = pf.readlines()
@@ -90,93 +98,109 @@ def inputSVM(infile):
     listID = lines[0::3]
     listaa = lines[1::3]
     listTop = lines[2::3]
+        
+    #return listaa    #returns difeerent string sets not sublist
+    """for element in dictAll.values():
+        aa_seq = element[0]
+        topology = element[1]"""
+        
+    window_input = 0
+    while window_input%2==0:
+        window_input = int(input("Window size (must be odd number): "))
     
-    window = int(input("Window size (must be odd number): "))
+    x = window_input//2
     
 ###################################################################################
 # Convert into SVM input vector: Sliding Window Input for AA sequence
 ###################################################################################
+    print ("Breaking down sequences into windows...")
     
-    for AA_seq in listaa:
-        listAA = list(AA_seq)
-        for ch in range(0, len(listAA)-(window-1)): #instead of accounting for the null residue before and after, I account for it when I specify the range
-            AAlist.append(listAA[ch:ch+window])
-    
-    #return AAlist
-    #print (listaa)
-    
-###################################################################################
-# Convert into SVM input vector: Sliding Window Input for topology
-###################################################################################
-    
-    for structure_ in listTop:
-        listTOP = list(structure_)
-        for ch in range(0, len(listTOP)-(window-1)):
-            Toplist.append(listTOP[ch:ch+window])
-    
-    return Toplist
-    
+    #for element in listaa:
+    for zeroseq in listaa:
+        zeroseq = ((x)*'0')+zeroseq+((x)*'0')
+        for aa in range(0,len(zeroseq)):
+            window=zeroseq[aa:aa+window_input]
+            if len(window)==window_input:
+                listaa_window.append(window)
+    #return listaa_window           
+    #key = 0
+    #seq[i-x:i+x] 
 ###################################################################################
 # Convert into SVM input vector: AA sequence to binary
 ###################################################################################
     
-    """mappedprotein = []
-    map = {'0':0, 'A':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7,'I':8, 'K':9,'L':10,  'M':11, 'N':12, 'P':13, 'Q':14, 'R':15, 'S':16, 'T':17, 'V':18, 'W':19,'Y':20}
-    # 0 to be used for window (to make of equal size)
-    dictAA = parseBeta(infile)
-    #print (dictAA)
-    proNumerical = []
-    for element in dictAA:
-        for character in element:
-            proNumerical_ = map[character]
-            proNumerical.append(proNumerical_)
-            mappedprotein.append(proNumerical_)
-            
-    #integer_encoded= [char_int]
-
-    return mappedprotein
- 
-def onehotEncoder(infile):
-
-    values = array(inputData)
-    print(values)
-    # integer encode
-    label_encoder = LabelEncoder()
-    integer_encoded = label_encoder.fit_transform(values)
-    print(integer_encoded)
-    # binary encode
-    onehot_encoder = OneHotEncoder(sparse=False)
-    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-    print(onehot_encoded)
+    for aa in listaa_window:
+            for ch in aa: #in range or enumerate
+                if ch in AA_seq_dict.keys():
+                #for key in aa_seq:
+                    #listaa_window[sequence][aa] = aa_seq[key]
+                    AAlist.append(AA_seq_dict[ch]) #[(key)]
+                # ** expanding dictionaries into k,v pairs
+                #AAlist.extend(AA_seq_dict.get(key))
+            final_AAlist.append(np.array(AAlist))
+    #return final_AAlist
     
-    dv = DictVectorizer(sparse=False)
-    file1 = inputSVM(infile)
-    df = file1.DataFrame(M).convert_objects(convert_numeric=True)
-    x = dv.fit_transform(df)
-    print (x)"""
- 
-
-
+###################################################################################
+# Convert into SVM input vector: Sliding Window Input for topology
+###################################################################################
+    print ("Breaking down topology into windows...")
+    
+    for lineT in listTop:
+        for ch in range (0, len(lineT)):
+            window=lineT[ch:ch+window_input]
+            if len(window)==window_input:
+                listTop_window.append(window)
+            
+    #return listTop_window  
+            
+###################################################################################
+# Convert into SVM input vector: topology to numerical
+###################################################################################
+            
+    for ch in listTop_window:
+        if window_input != 1:
+            middle = ch[x]
+            final_Toplist.append(structure_dict[middle])
+        else:
+            final_Toplist.append(structure_dict[ch])
+            
+    #return final_Toplist            
+    
 ###################################################################################
 # One-Hot Encoder
+###################################################################################    
+    
+    for sequence in listaa_window:
+        for aa in sequence:
+            enc = OneHotEncoder(sparse=False)
+            enc_window = enc.fit_transform(aa).toarray()
+            encwindow_list.append(enc_window)
+        encwindow_list = np.array(encwindow_list)
+    return encwindow_list
+    
+    listTop_window = np.array(listTop_window)
+    #return listTop_window
+    
+    #return encwindow_list, listTop_window
+    
+    """for value in listaa_window:
+        letter=[0 for _ in range(len(AA_seq_dict))]
+        letter[value]=1
+        encwindow_list.append(letter)
+    return encwindow_list"""
+    
+   #each training example has to be 1D 
 ###################################################################################
- 
+# Cross-validation
+###################################################################################    
 
-    onehot_encoder = OneHotEncoder(sparse=False)
-    integer_encoded = inputSVM(infile).reshape(len(inputSVM(infile)), 1)
-    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-    print(onehot_encoded)
+
+
     
 if __name__ == '__main__':
 
     #print(parseBeta("../datasets/parsetest"))
-    #print(onehotEncoder("../datasets/parsetest"))
     print(inputSVM("../datasets/parsetest"))
-    
-    
-    
-    
     
     
     
