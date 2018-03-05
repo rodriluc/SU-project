fecd574f9ef97dfa6ab2b989613fe0a5ba952f08
rodriluc <luciest.rodriguez@gmail.com>
2018-03-05 11:06:33 +0100

    fits an SVM model and saves it as TTmodel.sav

---
+++ projects/membrane-beta_4state/codes/trained_model.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.externals import joblib
+from numpy import array
+from sklearn import svm
+
+endfile = np.load('testfile.npz')
+x = endfile['arr_0.npy']
+y = endfile['arr_1.npy']
+clf_model = svm.SVC(gamma=0.001, kernel = 'linear', C=1.0).fit(x,y) 
+
+inputfile = 'TTmodel.sav'
+joblib.dump(clf_model, "inputfile")
